4be3f2f2bdc5de6aa6029e659ce209ff7ff65356
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2022-11-28 20:33:40 +0000

    dry out assertions
    This commit removes a lot of code duplication within the
    assert_xxx functions.

--- R/assert.R
+++ R/assert.R
@@ -38,244 +38,206 @@
 NULL
 
 # -------------------------------------------------------------------------
-# General assertions ------------------------------------------------------
-# -------------------------------------------------------------------------
-
 #' @rdname assertions
 #' @export
 assert_integer <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!is.integer(x)) {
-        msg <- sprintf("`%s` must be an integer vector.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!is.integer(x))
+        stopf("`%s` must be an integer vector.", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_int <- assert_integer
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_double <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!is.double(x)) {
-        msg <- sprintf("`%s` must be a double vector.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!is.double(x))
+        stopf("`%s` must be a double vector.", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_dbl <- assert_double
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_numeric <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!is.numeric(x)) {
-        msg <- sprintf("`%s` must be a numeric vector.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!is.numeric(x))
+        stopf("`%s` must be a numeric vector.", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_num <- assert_numeric
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_logical <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!is.logical(x)) {
-        msg <- sprintf("`%s` must be a logical vector.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!is.logical(x))
+        stopf("`%s` must be a logical vector.", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_lgl <- assert_logical
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_character <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!is.character(x)) {
-        msg <- sprintf("`%s` must be a character vector.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!is.character(x))
+        stopf("`%s` must be a character vector.", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_chr <- assert_character
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_data_frame <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!is.data.frame(x)) {
-        msg <- sprintf("`%s` must be a data frame.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!is.data.frame(x))
+        stopf("`%s` must be a data frame.", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_list <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!is.list(x)) {
-        msg <- sprintf("`%s` must be a list.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!is.list(x))
+        stopf("`%s` must be a list.", arg, call = call)
 }
 
 # -------------------------------------------------------------------------
-# Scalar assertions -------------------------------------------------------
-# -------------------------------------------------------------------------
-
 #' @rdname assertions
 #' @export
 assert_scalar_integer <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!(is.integer(x) && length(x) == 1)) {
-        msg <- sprintf("`%s` must be an integer vector of length 1.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!(is.integer(x) && length(x) == 1))
+        stopf("`%s` must be an integer vector of length 1.", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_scalar_int <- assert_scalar_integer
 
-
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_scalar_double <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!(is.double(x) && length(x) == 1)) {
-        msg <- sprintf("`%s` must be a double vector of length 1.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!(is.double(x) && length(x) == 1))
+        stopf("`%s` must be a double vector of length 1.", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_scalar_dbl <- assert_scalar_double
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_scalar_numeric <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!(is.numeric(x) && length(x) == 1)) {
-        msg <- sprintf("`%s` must be a numeric vector of length 1.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!(is.numeric(x) && length(x) == 1))
+        stopf("`%s` must be a numeric vector of length 1.", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_scalar_num <- assert_scalar_numeric
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_scalar_logical <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!(is.logical(x) && length(x) == 1)) {
-        msg <- sprintf("`%s` must be a logical vector of length 1.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!(is.logical(x) && length(x) == 1))
+        stopf("`%s` must be a logical vector of length 1.", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_scalar_lgl <- assert_scalar_logical
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_bool <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!(is.logical(x) && length(x) == 1) || is.na(x)) {
-        msg <- sprintf("`%s` must be boolean (TRUE/FALSE).", arg)
-        stop(simpleError(msg, call[1L]))
-    }
-
+    if (!(is.logical(x) && length(x) == 1) || is.na(x))
+        stopf("`%s` must be boolean (TRUE/FALSE).", arg, call = call)
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_boolean <- assert_bool
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_scalar_character <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
-    if (missing(x)) {
-        msg <- sprintf("argument `%s` is missing, with no default.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    .assert_not_missing(x = x, arg = arg, call = call)
 
-    if (!(is.character(x) && length(x) == 1)) {
-        msg <- sprintf("`%s` must be a character vector of length 1.", arg)
-        stop(simpleError(msg, call[1L]))
-    }
+    if (!(is.character(x) && length(x) == 1))
+        stopf("`%s` must be a character vector of length 1.", arg, call = call)
 
 }
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_scalar_chr <- assert_scalar_character
 
+# -------------------------------------------------------------------------
 #' @rdname assertions
 #' @export
 assert_string <- function(x, arg = deparse(substitute(x)), call = sys.call(-1L)) {
     assert_scalar_chr(x = x, arg = arg, call = call)
+}
+
+
+# ------------------------------------------------------------------------- #
+# ------------------------------------------------------------------------- #
+# -------------------------------- INTERNALS ------------------------------ #
+# ------------------------------------------------------------------------- #
+# ------------------------------------------------------------------------- #
+
+.assert_not_missing <- function(x, arg, call) {
+    if (missing(x))
+        stopf("argument `%s` is missing, with no default.", arg, call = call)
 }
