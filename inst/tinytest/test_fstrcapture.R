ce39de53bda9ed92405a38ba8ded96a781311f26
Tim Taylor <tim.taylor@hiddenelephants.co.uk>
2024-02-02 22:14:00 +0000

    test: fstrcapture

---
+++ inst/tinytest/test_fstrcapture.R
@@ -0,0 +1,47 @@
+# from regexpr example ----------------------------------------------------
+notables <- c(
+    "  Ben Franklin and Jefferson Davis",
+    "\tMillard Fillmore",
+    "Bob",
+    NA_character_
+)
+
+regex <- "(?<first>[[:upper:]][[:lower:]]+) (?<last>[[:upper:]][[:lower:]]+)"
+proto = data.frame("", "")
+
+expect_identical(
+    utils::strcapture(regex, notables, proto, perl = TRUE),
+    fstrcapture(regex, notables, proto)
+)
+
+expect_identical(
+    utils::strcapture(regex, notables, proto, perl = TRUE),
+    fstrcapture(regex, notables, proto, perl = TRUE)
+)
+
+expect_identical(
+    utils::strcapture(regex, "", proto, perl = TRUE),
+    fstrcapture(regex, "", proto)
+)
+
+expect_identical(
+    utils::strcapture(regex, NA_character_, proto, perl = TRUE),
+    fstrcapture(regex, NA_character_, proto)
+)
+
+
+# from strcapture example -------------------------------------------------
+x <- "chr1:1-1000"
+pattern <- "(.*?):([[:digit:]]+)-([[:digit:]]+)"
+proto <- data.frame(chr=character(), start=integer(), end=integer())
+
+expect_identical(
+    strcapture(pattern, x, proto),
+    fstrcapture(pattern, x, proto, perl = FALSE)
+)
+
+expect_identical(
+    strcapture(pattern, x, proto, perl = TRUE),
+    fstrcapture(pattern, x, proto)
+)
+
